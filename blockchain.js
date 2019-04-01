a673bb9ba8eafedd1df2b7cb60055db6cb12e708
近藤岳晴 <takeharu.kondo@acompany-ac.com>
2019-04-01 16:00:23 +0900

    final commit

--- blockchain.js
+++ blockchain.js
@@ -1,26 +1,21 @@
+const sha256 = require('sha256');
+
 class Blockchain {
 
     constructor(nonce, previousBlockHash, hash){
         this.chain = [];
-        this.pendingTransaction = [];
+        this.pendingTransactions = [];
+        this.currentNodeUrl = '';
+        this.networkNodes = [];
 
-        const newBlock = {
-            index: this.chain.length +1,
-            timestamp: Date.now(),
-            transactions: this.pendingTransaction,
-            nonce: nonce,
-            hash: hash,
-            previousBlockHash: previousBlockHash
-        }
-
-        this.chain.push(newBlock);
+        this.createNewBlock(1, '0', '0');
     }
 
     createNewBlock(nonce, previousBlockHash, hash){
         this.chain.push({
             index: this.chain.length +1,
             timestamp: Date.now(),
-            transactions: this.pendingTransaction,
+            transactions: this.pendingTransactions,
             nonce: nonce,
             hash: hash,
             previousBlockHash: previousBlockHash
@@ -32,7 +27,7 @@ class Blockchain {
     }
 
     createNewTransaction(amount, sender, to){
-        this.pendingTransaction.push({
+        this.pendingTransactions.push({
             amount: amount,
             sender: sender,
             to: to
@@ -40,6 +35,22 @@ class Blockchain {
 
         return this.getLastBlock()['index'] + 1;
     }
+
+    hashBlock(previousBlockHash, currentBlockData, nonce){
+        const dataAsString = previousBlockHash + nonce.toString() + JSON.stringify(currentBlockData);
+        return sha256(dataAsString);
+    }
+
+    proofOfWork(previousBlockHash, currentBlockData){
+        let nonce = 0;
+        let hash = this.hashBlock(previousBlockHash, currentBlockData, nonce);
+        while (hash.substring(0,4) != '0000'){
+            nonce++;
+            hash = this.hashBlock(previousBlockHash, currentBlockData, nonce);
+        }
+        return nonce;
+    }
+
 }
 
 module.exports = Blockchain;
--- test.js
+++ test.js
@@ -1,9 +1,46 @@
 const Blockchain = require('./blockchain');
 
-const takecoin = new Blockchain(1111, 'qqqqqqqqq', 'wwwwwwwww');
-
-takecoin.createNewTransaction(11, 'erqwreqwer', 'qrweqwerq');
-
-takecoin.createNewBlock(111,'erqwerqwe','rqwerqwerq');
-
+const takecoin = new Blockchain();
 console.log(takecoin);
+
+takecoin.createNewTransaction(1000,'ALICE','BOB');
+
+function mining(takecoin){
+    const lastBlock = takecoin.getLastBlock();
+    const previousBlockHash = lastBlock['hash'];
+
+    const currentBlockData = {
+        transactions: takecoin.pendingTransactions,
+        index: lastBlock['index'] + 1
+    };
+
+    const nonce = takecoin.proofOfWork(previousBlockHash, currentBlockData);
+
+    const blockHash = takecoin.hashBlock(
+        previousBlockHash,
+        currentBlockData,
+        nonce
+    );
+
+    const newBlock = takecoin.createNewBlock(nonce, previousBlockHash, blockHash);
+}
+
+mining(takecoin);
+
+takecoin.createNewTransaction(
+    200,
+    "ALICE",
+    "BOB"
+   );
+   
+   mining(takecoin);
+   
+   takecoin.createNewTransaction(
+    300,
+    "BOB",
+    "ALICE"
+   );
+   
+   mining(takecoin);
+   
+   console.log(takecoin);
